dc1cc701ec48aa8bad5d9a35f99bf26d123a37d3
Angel Sanchez <josesanchez0297@gmail.com>
2024-08-28 22:48:55 -0500

    Pruebas de routas padre y rutas hijas

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,21 +1,39 @@
-import { Component, NgModule } from '@angular/core';
+import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { Page404Component } from './page404/page404.component';
+import { HomeChildOneComponent } from './home-child-one/home-child-one.component';
+import { HomeChildTwoComponent } from './home-child-two/home-child-two.component';
+import { HomeComponent } from './home/home.component';
 import { TestComponentComponent } from './test-component/test-component.component';
 
 const routes: Routes = [
   {
-    path:'', //TODO: http://localhost:4200/
-    component: TestComponentComponent
-  },
-  {
-    path:'test-route',
-    component: TestComponentComponent
-  },
-  {
-    path: '**',
-    redirectTo: '/test-route'
+    path:'',
+    component:HomeComponent,
+    children:[
+      {
+        path:'one',
+        component:HomeChildOneComponent
+      },
+      {
+        path:'two',
+        component:HomeChildTwoComponent
+      }
+    ]
   }
+
+
+  // {
+  //   path:'', //TODO: http://localhost:4200
+  //   component: TestComponentComponent
+  // },
+  // {
+  //   path:'test-route',
+  //   component: TestComponentComponent
+  // },
+  // {
+  //   path:'**',
+  //    redirectTo: '/test-route'
+  // }
 ];
 
 @NgModule({
