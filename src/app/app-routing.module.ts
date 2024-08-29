a238b7495e3d9ba088cb0097de7b25ca69c5d7b4
Angel Sanchez <josesanchez0297@gmail.com>
2024-08-29 14:56:25 -0500

    Prueba de proteccion de rutas con guardian

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,14 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ValidateSessionGuard } from './core/guards/validate-session.guard';
 
 const routes: Routes = [
   {
-    path:'auth', //TODO: http://localhost:4200/auth
+    path:'auth', // PUBLIC! //TODO: http://localhost:4200/auth
     loadChildren: () => import('./modules/auth/auth.module').then((m) => m.AuthModule)
   },
   {
-    path:'taks', //TODO: http://localhost:4200/task
-    loadChildren: () => import('./modules/task/task.module').then((m) => m.TaskModule)
+    path:'task', // PRIVATE!! //TODO: http://localhost:4200/task
+    loadChildren: () => import('./modules/task/task.module').then((m) => m.TaskModule),
+    canActivate:[ValidateSessionGuard]
   }
 ];
 
