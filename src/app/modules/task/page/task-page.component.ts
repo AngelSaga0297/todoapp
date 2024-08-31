87c26800d75aa47dac3f4c5d7f84369487067ff1
Angel Sanchez <josesanchez0297@gmail.com>
2024-08-31 18:06:31 -0500

    generacion y prueba de interceptor con token

--- src/app/modules/task/page/task-page.component.ts
+++ src/app/modules/task/page/task-page.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit, OnDestroy } from '@angular/core';
 import { interval, Observable, Subscription } from 'rxjs';
+import { TaskService } from '../services/task.service';
 
 @Component({
   selector: 'app-task-page',
@@ -13,25 +14,32 @@ export class TaskPageComponent implements OnInit, OnDestroy {
   groups:Array<any> = [];
   listObservables$:Array<Subscription> = [];
 
-  constructor() { }
+  constructor(private taskService:TaskService) { }
 
-  listUser:Array<{id:string, name:string}> = [
-    {
-      id: '1',
-      name:"Juan"
-    },
-    {
-      id: '2',
-      name:"Mario"
-    },    
-    {
-      id: '3',
-      name:"Sergio"
-    },
-  ]
+  // listUser:Array<{id:string, name:string}> = [
+  //   {
+  //     id: '1',
+  //     name:"Juan"
+  //   },
+  //   {
+  //     id: '2',
+  //     name:"Mario"
+  //   },    
+  //   {
+  //     id: '3',
+  //     name:"Sergio"
+  //   },
+  // ]
 
   //SOY EL INIT
   ngOnInit(): void {
+    this.taskService.getTask()
+    .subscribe((response) => {
+      console.log('---> ', response)
+    },(err) => {
+      console.log('Error', err)
+    })
+
 
     // const observer1$ = interval(1000).subscribe((res) => {
     //   console.log('👌 Llamando');
@@ -39,54 +47,54 @@ export class TaskPageComponent implements OnInit, OnDestroy {
 
     // this.listObservables$ = [observer1$];
 
-    this.groups = [
-      {
-        label: 'Nuevos',
-        color: 'tomato',
-        list: [
-          {
-            order: 'Como instalar Angular',
-            items: [
-              {
-                key: 'price',
-                value: 152
-              },
-              {
-                key: 'time',
-                value: 152
-              },
-              {
-                key: 'author',
-                value: {
-                  name: 'Angel Sanchez',
-                  avatar: 'https://avatars.githubusercontent.com/u/145235792?v=4'
-                }
-              },
-            ]
-          },
-          {
-            order: 'Instalar Node',
-            items: [
-              {
-                key: 'price',
-                value: 152
-              },
-              {
-                key: 'time',
-                value: 152
-              },
-              {
-                key: 'author',
-                value: {
-                  name: 'Angel Sanchez',
-                  avatar: 'https://avatars.githubusercontent.com/u/145235792?v=4'
-                }
-              },
-            ]
-          }
-        ]
-      }
-    ]
+    // this.groups = [
+    //   {
+    //     label: 'Nuevos',
+    //     color: 'tomato',
+    //     list: [
+    //       {
+    //         order: 'Como instalar Angular',
+    //         items: [
+    //           {
+    //             key: 'price',
+    //             value: 152
+    //           },
+    //           {
+    //             key: 'time',
+    //             value: 152
+    //           },
+    //           {
+    //             key: 'author',
+    //             value: {
+    //               name: 'Angel Sanchez',
+    //               avatar: 'https://avatars.githubusercontent.com/u/145235792?v=4'
+    //             }
+    //           },
+    //         ]
+    //       },
+    //       {
+    //         order: 'Instalar Node',
+    //         items: [
+    //           {
+    //             key: 'price',
+    //             value: 152
+    //           },
+    //           {
+    //             key: 'time',
+    //             value: 152
+    //           },
+    //           {
+    //             key: 'author',
+    //             value: {
+    //               name: 'Angel Sanchez',
+    //               avatar: 'https://avatars.githubusercontent.com/u/145235792?v=4'
+    //             }
+    //           },
+    //         ]
+    //       }
+    //     ]
+    //   }
+    // ]
 
   }
 
